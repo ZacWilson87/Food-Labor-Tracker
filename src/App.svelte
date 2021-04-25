07b817ea9551f9edb17fb9947717641a13823b9e
Zac <zilson19878@gmail.com>
2021-04-25 15:13:59 -0500

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Landing from "./components/landing-page.svelte";
+</script>
+
+<main>
+  <Landing />
+</main>
+
+<style>
+</style>
---
+++ src/components/landing-page.svelte
@@ -0,0 +1,41 @@
+<script>
+</script>
+
+<img src="images/bg-image.jpeg" alt="pizza" class="bg-image" />
+<div class="login-frame">
+  <h2>Food & Labor Tracker</h2>
+  <button class="login">Enter</button>
+</div>
+
+<style>
+  img {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+  .login-frame {
+    background-color: rgba(255, 255, 255, 0.45);
+    position: fixed;
+    left: 15%;
+    top: 10%;
+    width: 25%;
+    height: 25%;
+    border-radius: 0.25em;
+    text-align: center;
+  }
+  .login {
+    position: relative;
+    background-color: rgb(65, 105, 1);
+    border: 1%;
+    color: black;
+    border-color: black;
+    border-radius: 0.5em;
+    top: 50%;
+    width: 25%;
+  }
+  .login:hover {
+    background-color: rgb(114, 185, 0);
+  }
+</style>
